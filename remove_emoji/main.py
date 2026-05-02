e32f1cf482122988b4f48d14fe3e208870c8aeee
bag1s3k <jetam.mravenec@gmail.com>
2026-05-02 23:26:00 +0200

    Customize style of the badge

--- remove_emoji/main.py
+++ remove_emoji/main.py
@@ -18,6 +18,7 @@ with open("README.md", "r+", encoding="utf-8") as f:
     highlight_emoji = emoji.replace_emoji(text, replace=custom_emoji_replace)
     clean_text = highlight_emoji.replace("@@@EMOJI@@@ ", "") # emoji + space
     clean_text = clean_text.replace("@@@EMOJI@@@", "") # emoji without space (e.g. emojis next to each other)
+    clean_text = clean_text.replace("style=flat", "style=for-the-badge&color=white")
 
     f.write(clean_text)
 
