c6d218a19810fc3af43dc6e7d974a19ee0597aa6
bag1s3k <jetam.mravenec@gmail.com>
2026-05-02 22:00:55 +0200

    Fix case where emojis are right next to each other

--- remove_emoji/main.py
+++ remove_emoji/main.py
@@ -10,13 +10,14 @@ def custom_emoji_replace(emoji_char, data_dict):
     return "@@@EMOJI@@@"
 
 
-with open("README.md", "r+", encoding="utf-8") as f:
+with open("../README.md", "r+", encoding="utf-8") as f:
     text = f.read()
     f.seek(0)
     f.truncate()
 
     highlight_emoji = emoji.replace_emoji(text, replace=custom_emoji_replace)
-    clean_text = highlight_emoji.replace("@@@EMOJI@@@ ", "")
+    clean_text = highlight_emoji.replace("@@@EMOJI@@@ ", "") # emoji + space
+    clean_text = clean_text.replace("@@@EMOJI@@@", "") # emoji without space (e.g. emojis next to each other)
 
     f.write(clean_text)
 
